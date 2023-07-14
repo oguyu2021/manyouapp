fc2834023d654aa607573835092f8296d9a67e51
ogurayusuke <oguyu2021@gmail.com>
2023-07-14 16:14:16 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
  resources :tasks
  root to: 'tasks#index'
+ resources :users, only: [:new, :create, :show]
+ resources :sessions, only: [:new, :create, :destroy]
 end
